a6dd8d6c9f1578b610c09d80bd300149b1d0ae71
Seren_Modz 21 <seren@kings-world.net>
2021-12-14 23:10:16 +0000

    :fire: delete old migration file

--- prisma/migrations/20211110062833_db/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "sticky_messages" (
-    "message_id" TEXT NOT NULL,
-    "channel_id" TEXT NOT NULL,
-    "data" JSONB NOT NULL DEFAULT E'{}',
-
-    CONSTRAINT "sticky_messages_pkey" PRIMARY KEY ("message_id")
-);
