76f669b73a7fce3f0e9a02849fac234f86eb0e87
Seren_Modz 21 <seren@kings-world.net>
2021-12-14 23:10:52 +0000

    :bulb: add comments

--- src/core/buttons/closeTicket.ts
+++ src/core/buttons/closeTicket.ts
@@ -6,18 +6,21 @@ export default class CloseTicket extends Button {
   async exec(interaction: ButtonInteraction) {
     await interaction.deferReply();
 
+    // match the ticket id with the channel name
     const id = getTicketId((<BaseGuildTextChannel>interaction.channel).name);
     if (!id)
       return interaction.editReply(
         "Sorry, I was unable to match an ID in the channel name!"
       );
 
+    // check if the id is in the database
     const ticket = await prisma.tickets.findUnique({ where: { id } });
     if (!ticket)
       return interaction.editReply(
         "Sorry, I was unable to find a record in the database!"
       );
 
+    // set the channel to be closed
     await prisma.tickets.update({
       where: { id: ticket.id },
       data: { closed: true },
--- src/core/buttons/deleteTicket.ts
+++ src/core/buttons/deleteTicket.ts
@@ -8,27 +8,29 @@ export default class DeleteTicket extends Button {
   async exec(interaction: ButtonInteraction) {
     await interaction.deferReply();
 
+    // check if the user has manage channel perms
     if (!interaction.memberPermissions?.has("MANAGE_CHANNELS"))
       return interaction.editReply(
         "Sorry, you are missing the required permissions to delete the ticket!"
       );
 
+    // match the ticket id with the channel name
     const id = getTicketId((<BaseGuildTextChannel>interaction.channel).name);
     if (!id)
       return interaction.editReply(
         "Sorry, I was unable to match an ID in the channel name!"
       );
 
+    // check if the id is in the database
     const ticket = await prisma.tickets.findUnique({ where: { id } });
     if (!ticket)
       return interaction.editReply(
         "Sorry, I was unable to find a record in the database!"
       );
 
+    // reply and 5 seconds later, delete the channel
     await interaction.editReply("The ticket will be deleted in 5 seconds.");
     await wait(5000);
-    return interaction
-      .channel!.delete()
-      .catch((e) => interaction.followUp(e.message));
+    return interaction.channel!.delete();
   }
 }
