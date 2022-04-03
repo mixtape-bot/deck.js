ce6ddc741f8a7395babcaeb59e286f0adb6be2d1
Seren_Modz 21 <seren@kings-world.net>
2022-04-03 09:33:24 +0100

    :art: clean up the if statement

--- src/core/listeners/client/interaction.ts
+++ src/core/listeners/client/interaction.ts
@@ -6,17 +6,17 @@ import type { Interaction } from "discord.js";
 })
 export default class InteractionCreate extends Listener {
   async exec(interaction: Interaction) {
-    if (interaction.isButton()) {
-      try {
-        return await this.client.buttons.modules
-          .get(interaction.customId)
-          ?.exec(interaction);
-      } catch (e) {
-        this.logger.error(e);
-        const func =
-          interaction.replied || interaction.deferred ? "editReply" : "reply";
-        return interaction[func](`Oops, an error occurred. **${e}**`);
-      }
+    if (!interaction.isButton()) return;
+
+    try {
+      return await this.client.buttons.modules
+        .get(interaction.customId)
+        ?.exec(interaction);
+    } catch (e) {
+      this.logger.error(e);
+      const func =
+        interaction.replied || interaction.deferred ? "editReply" : "reply";
+      return interaction[func](`Oops, an error occurred. **${e}**`);
     }
   }
 }
